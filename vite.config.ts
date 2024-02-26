a36c771c7fcf2f045f6030396f5dc505dddb8841
laihanbin <512412344@qq.com>
2024-02-26 17:03:11 +0800

    chore: init project

---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import { resolve } from 'path'
+import { loadEnv, defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, __dirname)
+  return {
+    base: env.VITE_APP_PATHNAME,
+    test: {},
+    plugins: [
+      vue(),
+      AutoImport({
+        include: [
+          /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+          /\.vue$/, /\.vue\?vue/, // .vue
+          /\.md$/ // .md
+        ],
+        imports: [
+          'vue',
+          'vue-router',
+          'pinia'
+        ],
+        eslintrc: {
+          enabled: true
+        }
+      }),
+      Components({
+        resolvers: [
+          AntDesignVueResolver({
+            resolveIcons: true,
+            importStyle: false
+          })
+        ]
+      })
+    ],
+
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src')
+      }
+    }
+  }
+})
